c70161f6346ad0706d851d74ed8fad0bd3fa7076
Adrian Miriuta <a.miriuta@ledvance.com>
2018-02-08 11:44:05 +0100

    qAcc 1004...

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -382,7 +382,7 @@ static void imuMahonyAHRSupdate(float dt, float gx, float gy, float gz,
 
     //quaternionCopy(&qAcc, &qAttitude);
 
-    quaternionSlerp(&qAcc, &qGyro,  &qAttitude, 0.991);
+    quaternionSlerp(&qAcc, &qGyro,  &qAttitude, 0.999);
 
     quaternionNormalize(&qAttitude);
     quaternionCopy(&qAttitude, &qGyro);
