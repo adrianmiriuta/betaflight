caaa57b4502bf15c1a5e7dff89778b99c3ab1b11
Adrian Miriuta <a.miriuta@ledvance.com>
2018-02-08 09:13:33 +0100

    qAcc add qGyroYaw

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -358,9 +358,8 @@ static void imuMahonyAHRSupdate(float dt, float gx, float gy, float gz,
 
     quaternion qAccPitch;
     //const float xz2 = atan2_approx(-vAcc.x, sqrtf(vAcc.y * vAcc.y + vAcc.z * vAcc.z) )/2; // ROT xyz
-    const float pitch = acos_approx(sqrtf(vAcc.y * vAcc.y + vAcc.z * vAcc.z)/sqrtf(vAcc.x * vAcc.x + vAcc.y * vAcc.y + vAcc.z * vAcc.z) )/2; // ROT xyz
-    constrainf(pitch, -M_PIf2, M_PIf2);
-    const float pitch2 = pitch/2f;
+    const float pitch2 = acos_approx(sqrtf(vAcc.y * vAcc.y + vAcc.z * vAcc.z)/sqrtf(vAcc.x * vAcc.x + vAcc.y * vAcc.y + vAcc.z * vAcc.z) )/2; // ROT xyz
+    constrainf(pitch2, 0.0f, M_PIf2);
 
     qAccPitch.w = cos_approx(pitch2);
     qAccPitch.x = 0;
