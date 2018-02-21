a967a0b48947dc372b729d947941d201e9e1c13d
Adrian Miriuta <a.miriuta@ledvance.com>
2018-02-21 14:37:46 +0100

    test acc +

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -488,7 +488,7 @@ static void imuMahonyAHRSupdate(float dt, float gx, float gy, float gz,
       qAcc.w = vAcc.y/sqrtf(2.0f * (1 - vAcc.z));
       qAcc.x = sqrtf((1 - vAcc.z)/2.0f);
       qAcc.y = 0;
-      qAcc.z = +vAcc.x/sqrtf(2.0f * (1 - vAcc.z));
+      qAcc.z = -vAcc.x/sqrtf(2.0f * (1 - vAcc.z));
     }
     quaternionComputeProducts(&qAcc, &qpAcc);
 
