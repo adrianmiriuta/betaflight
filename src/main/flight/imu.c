75bd48ececba39edcfa1c8d0c66799e7e3cfe852
Adrian Miriuta <a.miriuta@ledvance.com>
2018-03-22 14:06:30 +0100

    debug 2

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -88,8 +88,6 @@ STATIC_UNIT_TESTED quaternionProducts qpAttitude = QUATERNION_PRODUCTS_INITIALIZ
 quaternion qHeadfree = QUATERNION_INITIALIZE;
 quaternion qOffset = QUATERNION_INITIALIZE;
 
-static float vGyroModulusOld = 0.001;
-
 // absolute angle inclination in multiple of 0.1 degree    180 deg = 1800
 attitudeEulerAngles_t attitude = EULER_INITIALIZE;
 
@@ -273,6 +271,7 @@ static void imuMahonyAHRSupdate(float dt, quaternion *vGyro, bool useAcc, quater
 
     // vGyro integration
     // PCDM Acta Mech 224, 3091–3109 (2013)
+    static float vGyroModulusOld = 0;
     const float vGyroModulus = quaternionModulus(vGyro);
     // reduce gyro noise integration integrate only differences above vGyroStdDevModulus
     if (fabsf(vGyroModulus - vGyroModulusOld) > vGyroStdDevModulus) {
