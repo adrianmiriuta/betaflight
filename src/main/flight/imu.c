39823b22ce017d65a4ace98a27a6d7ce7b865ee0
Adrian Miriuta <a.miriuta@ledvance.com>
2018-03-23 06:39:46 +0100

    cleanup

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -273,6 +273,8 @@ static void imuMahonyAHRSupdate(float dt, quaternion *vGyro, bool useAcc, quater
     static float vGyroModulusOld = 0;
     const float vGyroModulus = quaternionModulus(vGyro);
     // reduce gyro noise integration integrate only differences above vGyroStdDevModulus
+    //debug
+    DEBUG_SET(DEBUG_IMU, DEBUG_IMU_FREE, lrintf(fabsf(vGyroModulus - vGyroModulusOld) * 1000));
     if ((vGyroModulus > vGyroStdDevModulus) && (fabsf(vGyroModulus - vGyroModulusOld) > vGyroStdDevModulus)) {
         quaternion qDiff;
         vGyroModulusOld = vGyroModulus;
@@ -303,7 +305,6 @@ static void imuMahonyAHRSupdate(float dt, quaternion *vGyro, bool useAcc, quater
     //debug
     DEBUG_SET(DEBUG_IMU, DEBUG_IMU_VGYROMODULUS, lrintf(vGyroModulus * 1000));
     DEBUG_SET(DEBUG_IMU, DEBUG_IMU_VKPKIMODULUS, lrintf(vKpKiModulus * 1000));
-    DEBUG_SET(DEBUG_IMU, DEBUG_IMU_FREE, lrintf(fabsf(vGyroModulus - vGyroModulusOld) * 1000));
     //DEBUG_SET(DEBUG_IMU, DEBUG_IMU_FREE, lrintf(quaternionModulus(&qAttitude) * 1000));
     //DEBUG_SET(DEBUG_IMU, DEBUG_IMU_FREE, lrintf(vGyroStdDevModulus * 1000));
 }
